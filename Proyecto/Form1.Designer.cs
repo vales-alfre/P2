83b3108161251471b7f9912522459cd271caffa9
vales-alfre <97996152+vales-alfre@users.noreply.github.com>
2022-01-18 21:17:11 -0500

    agregar label

--- Proyecto/Form1.Designer.cs
+++ Proyecto/Form1.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro\r\n\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(697, 419);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
